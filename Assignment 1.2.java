82cd82bdbcdb8746bf4e0edd5038d0a0d11fe727
LumpiaNation <39897299+LumpiaNation@users.noreply.github.com>
2019-03-19 13:02:53 -0700

    updates

---
+++ Assignment 1.2.java
@@ -0,0 +1,35 @@
+public class Assignment 1.2
+{
+    // Set A
+    // 1.
+    // Output == How do you doSeven of Nine
+
+    // 2.
+    // Output == 7
+
+    // 3.
+    // Output == defg
+
+    // 4.
+    // Output == 
+    //     abc
+    //     def
+
+    // 5.
+    // Output ==
+    //     abc\ndef
+    
+    // 6. 
+    // Output == HELLO JOHN
+
+    // Set B
+    // 1.
+    // Output == false
+
+    // 2.
+    // Output == false
+
+    // 3.
+    // Output == 
+
+}
